04049e729170cbae07fec3c3d287785bc075d194
AnneMaier <hhch98@gmail.com>
2025-06-10 05:33:21 +0000

    node js deploy

---
+++ hello/app.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res) {
+    res.json("hello node js")
+})
+
+
+app.listen(8000, function(){
+    console.log("server start")
+})
